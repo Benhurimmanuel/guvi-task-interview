6686ed45c58239b771e4f3060d5bb0f9db76bf9d
Benhur Immanuel <benhurimmanuel@gmail.com>
2021-11-15 17:12:19 +0530

    test

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include('./includes/header.php');?>
+
+<div class="container">
+
+    <div class="card m-5">
+        <h2 class="card-subtitle m-3">
+            Guvi task
+        </h2>
+        <div class="row card-text">
+            <div class="offset-1 col-5">
+                <a href="register.html"><button type="submit" class="btn">Register <i class="fa fa-user-plus"
+                            aria-hidden="true"></i></button></a>
+            </div>
+            <div class="col-5 mb-5">
+                <a href="login.html"> <button type="submit" class="btn">Login <i class="fa fa-user"
+                            aria-hidden="true"></i></button></a>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<?php include('./includes/footer.php');?>
+
+</body>
+
+</html>
